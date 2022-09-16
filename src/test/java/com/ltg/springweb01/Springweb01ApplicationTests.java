d7f473422910b68c1eb72bb4577c5e0b71270c48
ltgas <382422169@qq.com>
2022-09-16 09:00:54 +0800

    Initial commit

---
+++ src/test/java/com/ltg/springweb01/Springweb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltg.springweb01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springweb01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
